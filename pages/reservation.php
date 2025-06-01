e4b8b751dee56e093609b6b5b108588cb08e3492
kielbassa <karol.narel@icloud.com>
2025-06-01 13:19:25 +0200

    zmiana linkow miedzy stronami na php

--- pages/reservation.php
+++ pages/reservation.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="../css/reservation.css">
     <script src="../js/auth.js"></script>
   </head>
-  
+
 <body>
   <!-- logo i nazwa -->
   <header class="header-section">
@@ -28,24 +28,24 @@
         <span>Strefa Strzału Gdynia</span>
       </div>
     </div>
-  
+
     <div class="logo-container">
       <img src="../zdj/logo.png" alt="Logo" class="logo">
     </div>
-  
+
     <div class="auth-links">
-      <a href="register.html" class="auth-link" id="register-link">Rejestracja</a>
-      <a href="login.html" class="auth-link" id="login-link">Zaloguj się</a>
+      <a href="register.php" class="auth-link" id="register-link">Rejestracja</a>
+      <a href="login.php" class="auth-link" id="login-link">Zaloguj się</a>
     </div>
     </header>
-      
-  
+
+
   <!-- pasek nawigacyjny -->
   <nav class="main-nav">
     <a href="../pages/index.html" class="nav-link" id="home-link">Strona Główna</a>
-    <a href="../pages/strzelnica.html" class="nav-link">Strzelnica</a>
-    <a href="../pages/reservation.html" class="nav-link">Rezerwacje</a>
-    <a href="../pages/store.html" class="nav-link">Sklep</a>
+    <a href="../pages/strzelnica.php" class="nav-link">Strzelnica</a>
+    <a href="../pages/reservation.php" class="nav-link">Rezerwacje</a>
+    <a href="../pages/store.php" class="nav-link">Sklep</a>
     <a href="../pages/kontakt.html" class="nav-link">Kontakt</a>
   </nav>
 
@@ -64,7 +64,7 @@
     <p>
       Prosimy zabrać ze sobą <strong>dokument tożsamości</strong> oraz zapoznać się z <strong>regulaminem strzelnicy</strong> przed strzelaniem.
     </p>
-  
+
     <h3 class="pricing-title">Cennik</h3>
     <table class="pricing-table">
       <tr><td>Wynajem stanowiska</td><td>100 zł / h za osobę</td></tr>
@@ -74,22 +74,22 @@
       <tr><td>Zajęcia z instruktorem</td><td>150 zł / h za osobę</td></tr>
     </table>
   </div>
-  
-  
+
+
   <section class="reservation">
     <h2 class="reservation-title">Formularz rezerwacji</h2>
     <form id="reservation-form">
       <label for="date" class="form-label">Wybierz datę</label>
       <input type="date" id="date" name="date" required>
-  
+
       <label for="time" class="form-label">Wybierz godzinę</label>
       <select id="time" name="time" required>
         <option value="">-- wybierz godzinę --</option>
       </select>
-  
+
       <label for="people" class="form-label">Liczba osób</label>
       <input type="number" id="people" name="people" min="1" max="10" required>
-  
+
       <label for="instructor" class="form-label">Zajęcia z instruktorem?</label>
       <select id="group" name="group" required>
         <option value="nie">Nie</option>
@@ -101,19 +101,19 @@
         <option value="nie">Nie</option>
         <option value="tak">Tak</option>
       </select>
-      
-      
+
+
 
       <button type="submit">Zarezerwuj</button>
     </form>
   </section>
-  
-  
+
+
     <footer>
       <img src="../zdj/logo.png" alt="Logo">
       <p>Wszelkie prawa zastrzeżone dla strefastrzalu.pl © 2025 | Projekt i wykonanie chrust</p>
     </footer>
-    
+
     <script src="../js/buttons.js"></script>
     <script src="../js/navbar.js"></script>
     <script src="../js/date.js"></script>
